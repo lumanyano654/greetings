10e81003da8e2e191add8f4ccde814799e88ed2a
lumanyano654 <gomomolumanyano@gmail.com>
2020-07-23 13:45:33 +0200

    first commit

---
+++ greet.js
@@ -0,0 +1,46 @@
+const theText = document.querySelector(".textbox");
+const theButton = document.querySelector(".button");
+const radioButton = document.querySelector(".radioButton");
+const resultBox = document.querySelector(".return")
+
+
+
+var name = "";
+// var nameTyped = "hello , " + name
+
+
+function greet() {
+
+    var nameEntered = theText.value;
+    var language = document.querySelector("input[name='radioButton']:checked");
+//    alert(nameEntered === "")
+    //var langEntered = language;
+
+    if (nameEntered === "" && language.value === null){
+
+        resultBox.innerHTML = "please enter name and select language!!!"
+    }
+
+   else if (language.value === "isixhosa") {
+        resultBox.innerHTML = "Molo, " + nameEntered;
+
+     }
+
+    else if (language.value === "english"){
+        resultBox.innerHTML = "Hello, " + nameEntered
+    }
+
+    else if (language.value=== "afrikaans"){
+        resultBox.innerHTML = "Halo, " + nameEntered
+    }
+    
+    
+
+    // resultBox.innerHTML = "Molo, " + nameEntered;
+
+    // theText.innerHTML = name
+    // resultBox.innerHTML = nameTyped
+}
+theButton.addEventListener("click", greet);
+
+//alert(greet())
